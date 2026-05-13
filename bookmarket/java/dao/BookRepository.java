7ff6607dfafa9d7e53022927390a5353daede0e9
daseolgim18-cpu <daseolgim18@gmail.com>
2026-05-13 14:31:58 +0900

    Enhance BookRepository with filename and loop update
    Added filename property to books and updated getBookById loop.

--- bookmarket/java/dao/BookRepository.java
+++ bookmarket/java/dao/BookRepository.java
@@ -6,7 +6,8 @@ import dto.Book;
 public class BookRepository {
     
     private ArrayList<Book> listOfBooks = new ArrayList<Book>();
-    private static BookRepository instance=new BookRepository();
+
+    private static BookRepository instance = new BookRepository();
 
     public static BookRepository getInstance() {
         return instance;
@@ -24,6 +25,7 @@ public class BookRepository {
         book1.setCategory("IT모바일");
         book1.setUnitsInStock(1000);
         book1.setReleaseDate("2022/10/06");
+        book1.setFilename("ISBN1234.jpg");
 
         Book book2 = new Book("ISBN1235", "자바마스터", 30000);
         book2.setAuthor("송미영");
@@ -35,6 +37,7 @@ public class BookRepository {
         book2.setCategory("IT모바일");
         book2.setUnitsInStock(1000);
         book2.setReleaseDate("2023/01/01");
+        book2.setFilename("ISBN1235.jpg");
 
         Book book3 = new Book("ISBN1236", "파이썬 프로그래밍", 30000);
         book3.setAuthor("최성철");
@@ -46,6 +49,7 @@ public class BookRepository {
         book3.setCategory("IT모바일");
         book3.setUnitsInStock(1000);
         book3.setReleaseDate("2023/01/01");
+        book3.setFilename("ISBN1236.jpg");
 
         listOfBooks.add(book1);
         listOfBooks.add(book2);
@@ -57,15 +61,17 @@ public class BookRepository {
     }
 
     public Book getBookById(String bookId) {
+
         Book bookById = null;
 
-        for (int i = 0; i < listOfBooks.size(); i++){
-            Book book = listOfBooks.get(i);
-            if (book!=null && book.getBookId()!=null 
-                && book.getBookId().equals(bookId)) {
-                    bookById = book;
-                    break;
-                }
+        for (Book book : listOfBooks) {
+            if (book != null &&
+                book.getBookId() != null &&
+                book.getBookId().equals(bookId)) {
+
+                bookById = book;
+                break;
+            }
         }
         return bookById;
     }
